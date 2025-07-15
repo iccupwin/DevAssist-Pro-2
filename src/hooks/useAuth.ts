1aa47ffae02e6eab75a273094fe8628cbb9823d3
Bakhtiyar <35561658+iccupwin@users.noreply.github.com>
2025-07-15 21:14:17 +0600

    Delete src directory

--- src/hooks/useAuth.ts
+++
@@ -1,9 +0,0 @@
-import { useAuth as useAuthContext } from '../contexts/AuthContext';
-
-// Реэкспорт для удобства использования
-export const useAuth = useAuthContext;
-
-// Дополнительные хуки для специфичных случаев
-export { useAuthGuard } from './useAuthGuard';
-export { useSessionTimeout } from './useSessionTimeout';
-export { useAuthRedirect } from './useAuthRedirect';
--- src/pages/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import MainPage from './MainPage';
-
-// Экспорт главной страницы как индексной страницы
-export default MainPage;
